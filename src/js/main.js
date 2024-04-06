25f369c3c207202f153ed2f6b782a7a0c1bd1851
LeonardSMacedo <leodsdm@gmail.com>
2024-04-06 11:58:30 -0300

    fix: alteracoes home

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".bannerTopo .bannerContainer").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  });
+});
