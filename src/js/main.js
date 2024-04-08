57ef0e2fcda4d1e87f6871e796a664bd13714990
LeonardSMacedo <leodsdm@gmail.com>
2024-04-08 15:48:20 -0300

    fix: alteracao geral

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,23 @@ $(document).ready(function () {
     slidesToScroll: 1,
     autoplay: true
   });
+
+  $(".btnlogin").on("click", function (){
+  let login = $(".formLogin")
+  let cadastro = $(".formCad")
+
+  login.css("display", "block")
+  cadastro.css("display", "none")
 });
+
+$(".btncad").on("click", function (){
+  let login = $(".formLogin")
+  let cadastro = $(".formCad")
+
+  login.css("display", "none")
+  cadastro.css("display", "block")
+});
+
+});
+
+
